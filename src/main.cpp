a5eda6501c096a72762bd73d29e03d9fbae21f72
Evan Liu <evanyxliu@gmail.com>
2025-12-28 16:33:07 -0500

    basic wifi connection + dummy endpoint testing
    pretty sure this doesn't work in wokwi browser but it does work in vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,45 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-#define LED_PIN 2
+const char* ssid = "Wokwi-GUEST";
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
   Serial.println(GOTRANSIT_KEY);
+  WiFi.begin(ssid);
+  Serial.println("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
-  delay(1000); 
+  if (WiFi.status() == WL_CONNECTED) {
+
+    Serial.println("Sending http request");
+
+    HTTPClient http;
+
+    http.begin("https://jsonplaceholder.typicode.com/todos/1");
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+      Serial.println("Success");
+    } else {
+      Serial.print("Error on http request: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  } else {
+    Serial.println("Wi-Fi not connected");
+  }
+
+  delay(10000);
 }
